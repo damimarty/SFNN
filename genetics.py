f71f0e00b26be512637f29b1dde89b11af6eb5f3
damimarty <dam.marty@gmail.com>
2017-09-27 13:47:18 +0200

    Fornication implementation

--- genetics.py
+++ genetics.py
@@ -5,14 +5,20 @@
 from population import Population
 import random
 
+defaultNbIndividuals = 5
+defaultNbNeurons = 5
+defaultNbConnexions = 8
 class Genetics(object):
-    def __init__(self):
+    def __init__(self, populationN = None):
         self.pbaNewNeuron = 0.02
         self.pbaNewConnexion = 0.05
         self.pbaDeleteNeuron = 0.01
         self.pbaDeleteConnexion = 0.03
         self.elitism = 2
         self.wheelSize = 0.0
+        if populationN is None:
+            self.generationN = Population(defaultNbIndividuals,defaultNbNeurons,defaultNbConnexions)
+        else: self.generationN = populationN
         self.generationN = None
         self.generationNplusOne = None
 
@@ -29,7 +35,7 @@ class Genetics(object):
         for index in range(len(self.generationN.peopleList)):
             fit = self.generationN.peopleList[index].getFitness()
             if  fit + threshold > lot:
-                return index
+                return self.generationN.peopleList[index]
             threshold += fit
         return None
 
@@ -41,9 +47,23 @@ class Genetics(object):
             parent2 = self.drawLot()
         return parent1, parent2
 
-    def createNewGeneration(self):
-        parent1, parent2 = self.getParents()
-        child = fornication(parent1, parent2)
+    #def step(self):
+        #for i in range(self.elitism):
+            #copy best individuals
+        #for individual in self.generationN.peopleList:
+        #self.generationNplusOne.extend(self.generation.peopleList)
+        #parent1, parent2 = self.getParents()
+        #child = self.fornication(parent1, parent2)
+
+    def fornication(self, parent1, parent2):
+        neurons1, connexions1 = parent1.getGenes()
+        neurons2, connexions2 = parent2.getGenes()
+        split = int(random.random()*len(neurons1))
+        neuronsChild = neurons1[0:split]
+        neuronsChild.extend(neurons2[split:])
+        split = int(random.random()*len(connexions1))
+        connexionsChild = connexions1[0:split]
+        connexionsChild.extend(connexions2[split:])
 
 
 ofTheJungle = Population(4, 2, 2)
@@ -54,4 +74,5 @@ print(ofTheJungle)
 pool = Genetics()
 pool.getGenerationN(ofTheJungle)
 parent1, parent2 = pool.getParents()
+pool.fornication(parent1, parent2)
 print parent1, parent2
