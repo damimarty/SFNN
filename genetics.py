7b8d50ba09e98f85abb06eb537364024e90dd0a9
damimarty <dam.marty@gmail.com>
2017-09-27 16:11:39 +0200

    Cleaning of Genetics

--- genetics.py
+++ genetics.py
@@ -3,35 +3,32 @@
 # -*- coding: latin-1 -*-
 
 from population import Population
+from network import Network
 import random
-
+import matplotlib.pyplot as plt
 defaultNbIndividuals = 5
 defaultNbNeurons = 5
 defaultNbConnexions = 8
 class Genetics(object):
-    def __init__(self, populationN = None):
+    def __init__(self, nbPeople, nbNeu, nbConn, nbNeuInput, nbNeuOutput):
         self.pbaNewNeuron = 0.02
         self.pbaNewConnexion = 0.05
         self.pbaDeleteNeuron = 0.01
         self.pbaDeleteConnexion = 0.03
         self.elitism = 2
         self.wheelSize = 0.0
-        if populationN is None:
-            self.generationN = Population(defaultNbIndividuals,defaultNbNeurons,defaultNbConnexions)
-        else: self.generationN = populationN
-        self.generationN = None
+        self.generationN = Population(nbPeople, nbNeu, nbConn, nbNeuInput, nbNeuOutput)
+        self.computeWheelSize()
         self.generationNplusOne = None
 
-    def getGenerationN(self, populationN):
-        self.generationN = populationN
-        for index in range(len(populationN.peopleList)):
-            self.wheelSize += populationN.peopleList[index].getFitness()
+    def computeWheelSize(self):
+        for people in self.generationN.peopleList:
+            self.wheelSize += people.getFitness()
 
     def drawLot(self):
         parent = None
         threshold = 0.0
         lot = random.random()*self.wheelSize
-        print lot
         for index in range(len(self.generationN.peopleList)):
             fit = self.generationN.peopleList[index].getFitness()
             if  fit + threshold > lot:
@@ -43,7 +40,6 @@ class Genetics(object):
         parent1 = self.drawLot()
         parent2 = self.drawLot()
         while parent2 == parent1:
-            print "same"
             parent2 = self.drawLot()
         return parent1, parent2
 
@@ -64,15 +60,5 @@ class Genetics(object):
         split = int(random.random()*len(connexions1))
         connexionsChild = connexions1[0:split]
         connexionsChild.extend(connexions2[split:])
-
-
-ofTheJungle = Population(4, 2, 2)
-print(ofTheJungle)
-ofTheJungle.sortByFitness()
-print(ofTheJungle)
-
-pool = Genetics()
-pool.getGenerationN(ofTheJungle)
-parent1, parent2 = pool.getParents()
-pool.fornication(parent1, parent2)
-print parent1, parent2
+        child = Network(neuronsChild,connexionsChild,parent1.nbInputNeurons,parent1.nbOutputNeurons)
+        return child
