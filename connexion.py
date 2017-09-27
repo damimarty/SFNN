d065c96152c8dadf641e59a2b0b2f6132e507d41
damimarty <dam.marty@gmail.com>
2017-09-27 13:47:01 +0200

    Definition of print method

--- connexion.py
+++ connexion.py
@@ -30,3 +30,10 @@ class Connexion(object):
 
 	def getTo(self):
 		return self.To
+
+	def __str__(self):
+		if type(self.getFrom()) is int:
+			ret = "I am a connexion and I link "+str(self.getFrom()) + "\tto " + str(self.getTo()) + "\twith a force " + str(self.getW())
+		else:
+			ret = "I am a connexion and I link "+str(self.getFrom().getId()) + "\tto " + str(self.getTo().getId()) + "\twith a force " + str(self.getW())
+		return ret
