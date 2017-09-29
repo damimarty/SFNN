f8db1d164c24b28111ea63e81ac30ad100647a94
damimarty <marty.damien@yahoo.fr>
2017-09-29 12:33:54 +0200

    LatchRS improvement
    Latch RS now sees its output vary between 0.0 and 1.0.
    Also, a "problem" can now return a scenario, in order to allow static replay of the same scenario over and over again.

--- latchRS.py
+++ latchRS.py
@@ -9,28 +9,34 @@ import random
 
 class latchRS(object):
     def __init__(self):
-        self.output = 0.2
+        self.output = 0.5
 
     def run(self, inputArray):
         iSet, iReset = inputArray
         if iSet == 1.0 and iReset == 0.0:
-            self.output = 0.8
+            self.output = 1.0
         if iSet == 0.0 and iReset == 1.0:
-            self.output = 0.2
-        if iSet == 1.0 and iReset == 1.0:
-            if self.output == 0.8:
-                self.output = 0.2
-            else:
-                self.output = 0.8
+            self.output = 0.0
+        # if iSet == 1.0 and iReset == 1.0:
+            # if self.output == 1.0:
+                # self.output = 0.0
+            # else:
+                # self.output = 1.0
         return [self.output]
 
     def getInputs(self):
-        iSet = 1.0 if random.random() > 0.9 else 0.0
-        iReset = 1.0 if random.random() < 0.05 else 0.0
+        iSet = 1.0 if random.random() > 0.90 else 0.0
+        iReset = 1.0 if random.random() < 0.10 else 0.0
         return [iSet,iReset]
+        
+    def getScenario(self, size):
+        scenario = []
+        for i in range(size):
+            scenario.append(self.getInputs())
+        return scenario
 
     def error(self,o1,o2):
         if (len(o1) == 1) and (len(o2) == 1):
             v = abs(o1[0] - o2[0])
-            #return v**2
+            # return v**2
             return v
